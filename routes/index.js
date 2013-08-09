525cdf5d2973f1e255f8744d0e3cb6a884dcfeb3
brownie3003 <brownie3003@gmail.com>
2013-08-09 21:02:37 +0100

    about & contact

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index');
 };
+
+exports.about = function(req, res){
+	res.render('about');
+}
+
+exports.contact = function(req, res){
+	res.render('contact');
+}
