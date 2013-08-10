1808285c5b320705096ae145e710d3acc3a3a8f8
Nicholas Angeli <nicangeli@gmail.com>
2013-08-10 15:54:30 +0100

    calls

---
+++ models/call.js
@@ -0,0 +1,13 @@
+var client = require('twilio')('AC290d9a56b0ae27a9393a8e3b37e1df71', '45dab5b14cc90ad4e73a1975a0d93e4b');
+
+module.exports = function( ) {
+	this.makeCall = function() {
+		client.makeCall({
+			to: '+447847577561',
+			from: '+441733514667',
+			url: 'http://www.teachhub.co.uk/twiml.xml'
+		}, function(err,responseData) {
+			console.log(responseData);
+		});
+	}
+}
